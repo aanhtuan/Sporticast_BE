23d71925eb9ff4b5a2578a8a7ed51c37698b8961
quocvien0123 <quocvien0123@gmail.com>
2025-05-07 20:33:42 +0700

    merge

--- src/main/java/com/example/besporticast/Controller/AuthController.java
+++ src/main/java/com/example/besporticast/Controller/AuthController.java
@@ -2,12 +2,15 @@ package com.example.besporticast.Controller;
 
 import com.example.besporticast.DTO.Request.LoginRequest;
 import com.example.besporticast.DTO.Request.RegisterRequest;
-
-
 import com.example.besporticast.Service.UserService;
+import com.example.besporticast.Util.JwtUtil;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -16,8 +19,15 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @PostMapping("/register")
     public ResponseEntity<Map<String, String>> register(@Valid @RequestBody RegisterRequest request) {
@@ -25,15 +35,23 @@ public class AuthController {
         Map<String, String> response = new HashMap<>();
         response.put("message", result);
         return ResponseEntity.ok(response);
-
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login(@Valid @RequestBody LoginRequest request) {
-        boolean success = userService.loginUser(request.email, request.password);
-        Map<String, String> response = new HashMap<>();
-        response.put("message", success ? "Login Success" : "Login Failed");
-        return ResponseEntity.ok(response);
+    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequest request) {
+        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> loginData = userService.loginUser(request.email, request.password);
 
+        if (loginData != null) {
+            response.put("message", "Login Success");
+            response.put("token", loginData.get("token"));
+            response.put("is_admin", loginData.get("is_admin"));
+            return ResponseEntity.ok(response);
+        } else {
+            response.put("message", "Login Failed");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
     }
+
+
 }
