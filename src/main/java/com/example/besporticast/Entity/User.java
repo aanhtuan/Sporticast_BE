f06d4fa1d289d48a67bf24fde64ba2c295711200
quocvien0123 <quocvien0123@gmail.com>
2025-05-07 20:34:16 +0700

    merge

--- src/main/java/com/example/besporticast/Entity/User.java
+++ src/main/java/com/example/besporticast/Entity/User.java
@@ -3,10 +3,11 @@ package com.example.besporticast.Entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.time.Instant;
-
+@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -34,10 +35,9 @@ public class User {
     @Column(name = "avatar")
     private String avatar;
 
-    @ColumnDefault("'user'")
-    @Lob
-    @Column(name = "role")
-    private String role;
+    @NotNull
+    @Column(name = "is_admin", nullable = false)
+    private Boolean is_admin = false;
 
     @NotNull
     @ColumnDefault("current_timestamp()")
@@ -48,69 +48,5 @@ public class User {
     @ColumnDefault("current_timestamp()")
     @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 
 }
