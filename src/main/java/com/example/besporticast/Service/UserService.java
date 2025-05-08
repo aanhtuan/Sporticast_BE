7a1f2bbce71de30280159577b45627d99f9fba82
quocvien0123 <quocvien0123@gmail.com>
2025-05-09 04:54:38 +0700

    merge

--- src/main/java/com/example/besporticast/Service/UserService.java
+++ src/main/java/com/example/besporticast/Service/UserService.java
@@ -3,6 +3,7 @@ package com.example.besporticast.Service;
 import com.example.besporticast.Entity.User;
 import com.example.besporticast.Repository.UserRepository;
 import com.example.besporticast.Util.JwtUtil;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class UserService {
@@ -20,7 +22,10 @@ public class UserService {
     @Autowired
     private JwtUtil jwtUtil;
 
+
     public String registerUser(String name, String email, String password) {
+
+
         if (userRepository.existsByEmail(email)) {
             return "Email Already Exists";
         }
@@ -28,12 +33,19 @@ public class UserService {
         user.setName(name);
         user.setEmail(email);
         user.setPassword(passwordEncoder.encode(password));
+        user.setAvatar(getRandomAvatarUrl());
         user.setCreatedAt(Instant.now());
         user.setUpdatedAt(Instant.now());
         userRepository.save(user);
         return "User created successfully";
 
     }
+
+    private String getRandomAvatarUrl() {
+        int index = new Random().nextInt(70) + 1; // tạo số từ 1 đến 70
+        return "https://i.pravatar.cc/150?img=" + index;
+    }
+
 
     public Map<String, Object> loginUser(String email, String password) {
         Optional<User> userOpt = userRepository.findByEmail(email);
